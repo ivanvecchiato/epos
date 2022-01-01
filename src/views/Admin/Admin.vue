21dc2fad05f64e1b1b13753856048df96c244df0
ivan <ivan.vecchiato@gmail.com>
2022-01-01 19:43:52 +0100

    sezione admin con reset conti

---
+++ src/views/Admin/Admin.vue
@@ -0,0 +1,86 @@
+<template>
+   <div>
+      <el-button @click="generate">genera</el-button>
+      <el-button @click="resetOrders" v-loading.fullscreen.lock="fullscreenLoading">reset conti</el-button>
+   </div>
+</template>
+
+<script>
+import Order from "../../data/Order.js"
+import Firebase from "../../firebase.js"
+//import utils from "../../utils.js"
+
+export default {
+   name: 'Admin',
+   data() {
+      return {
+         fullscreenLoading: false
+      }
+   },
+   methods: {
+      resetOrders: function() {
+         this.fullscreenLoading = true;
+
+         Firebase.db.collection("park")
+         .get()
+         .then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+               var docId = doc.id;
+               var floor = doc.data().places;
+               for(var key in floor) {
+                  //console.log('resetOrders', utils.toDate(floor[key].order.createdAt));
+                  if(floor[key].order.orderList.length > 0) {
+                     var docRef = Firebase.db.collection('park').doc(docId);
+                     var place = "places." + key + ".order";
+                     docRef.update({
+                        [place]: Object.assign({}, new Order)
+                     })
+                     .then(() => {
+                       console.log("Document successfully written!");
+                     })
+                     .catch((error) => {
+                       console.error("Error writing document: ", error);
+                     });
+                  }
+               }
+            });
+            this.fullscreenLoading = false;
+         })
+         .catch((error) => {
+             console.log("Error getting documents: ", error);
+         });
+      },
+      generate: function() {
+        var tablesNum = [50, 20, 15, 10];
+        for(var j=0; j<4; j++) {
+          var roomObj = {
+            id: Number(j+1),
+            name: "Area " + (j+1),
+            places: []
+          };
+          for(var i=0; i<tablesNum[j]; i++) {
+            var obj = {
+              id: Number(i+1),
+              name: (i+1),
+              places: 2,
+              order: Object.assign({},new Order)
+            };
+
+            roomObj.places.push(obj);
+          }
+          Firebase.db.collection('park').add(roomObj)
+          .then((docRef) => {
+            console.log("Document written with ID: ", docRef.id)
+          })
+          .catch((error) => {
+            console.error("Error adding document: ", error)
+          })
+        }
+      }
+   },
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -6,8 +6,8 @@
     </el-tab-pane>
     <el-tab-pane label="Config">Config</el-tab-pane>
     <el-tab-pane label="Role">Role</el-tab-pane>
-    <el-tab-pane :label="$t('config.demo_data')">
-      <el-button @click="generate">genera</el-button>
+    <el-tab-pane :label="$t('config.admin')">
+      <admin></admin>
     </el-tab-pane>
   </el-tabs>
       
@@ -15,45 +15,18 @@
 </template>
 
 <script>
-import Order from "../data/Order.js"
-import Firebase from "../firebase.js"
 import Operators from "./Operators/Operators.vue"
+import Admin from "./Admin/Admin.vue"
 
 export default {
   name: "Settings",
-  components: { Operators },
+  components: { Operators, Admin },
   data() {
     return {
     };
   },
   methods: {
-    generate: function() {
-      var tablesNum = [50, 20, 15, 10];
-      for(var j=0; j<4; j++) {
-        var roomObj = {
-          id: Number(j+1),
-          name: "Area " + (j+1),
-          places: []
-        };
-        for(var i=0; i<tablesNum[j]; i++) {
-          var obj = {
-            id: Number(i+1),
-            name: (i+1),
-            places: 2,
-            order: Object.assign({},new Order)
-          };
 
-          roomObj.places.push(obj);
-        }
-        Firebase.db.collection('park').add(roomObj)
-        .then((docRef) => {
-          console.log("Document written with ID: ", docRef.id)
-        })
-        .catch((error) => {
-          console.error("Error adding document: ", error)
-        })
-      }
-    }
   },
   mounted() {
   },
