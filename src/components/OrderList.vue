567e1ff6f58787150cfefd6a7440fc06c5496f99
ivan <ivan.vecchiato@gmail.com>
2022-01-16 20:18:25 +0100

    componente per mostrare un ordine

---
+++ src/components/OrderList.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="cart">
+    <ul>
+    <li v-for="item in order.orderList" :key="item.id" class="cart-item">
+      <span class="item-quantity">{{item.quantity}}</span>
+      <span class="item-name">{{item.name}}</span>
+      <span class="item-price">{{formatPrice(item.price)}}</span>
+    </li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+import utils from '../utils.js'
+
+export default {
+  name: 'OrderList',
+  components: {},
+  props: ['order'],
+  data() {
+    return {
+    }
+  },
+  methods: {
+    formatPrice(p) {
+      return utils.formatPrice(p);
+    }
+  },
+  mounted() {
+    console.log(this.order);
+  },
+}
+</script>
+
+<style scoped>
+.cart {
+  text-align: left;
+}
+ul {
+  list-style-type: none;
+	list-style: none;
+	padding: 0;
+	width: 100%;
+}
+ul li {
+  margin: 15px;
+	padding: .5em 0;
+	border-bottom: 1px solid #CCC;
+  text-align: left;
+}
+ul li:last-child {
+	border-bottom: 0;
+}
+.item-quantity {
+  margin-right: 10px;
+  min-width: 40px;
+}
+.item-price {
+  float: right;
+}
+</style>
