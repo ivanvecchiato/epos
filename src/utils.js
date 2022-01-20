2a31bee89946a3f88406ac1815a36aff68c3208b
ivan <ivan.vecchiato@gmail.com>
2022-01-20 15:14:01 +0100

    formatPriceWithCurrency

--- src/utils.js
+++ src/utils.js
@@ -3,6 +3,9 @@ export default {
   formatPrice (amount) {
     return Number(amount).toFixed(2)
   },
+  formatPriceWithCurrency (amount) {
+    return Number(amount).toFixed(2) + " €"
+  },
   uuidv4() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
       var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
