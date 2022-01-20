211f9de4e48f037f15cdb6fc2f4dc5e33e71377b
ivan <ivan.vecchiato@gmail.com>
2022-01-20 22:29:58 +0100

    aggiunto arrayCompare

--- src/utils.js
+++ src/utils.js
@@ -78,5 +78,23 @@ export default {
     } else {
       return "";
     }
+  },
+  arrayCompare (array1, array2) {
+    // if the other array is a falsy value, return
+    if (!array1)
+      return false;
+    if (!array2)
+      return false;
+
+    if (array1.length != array2.length)
+      return false;
+
+    for (var i = 0, l=array1.length; i < l; i++) {
+      if (array1[i] != array2[i]) { 
+        // Warning - two different object instances will never be equal: {x:20} != {x:20} TODO!!!!
+        return false;   
+      }           
+    }       
+    return true;
   }
 }
