224f5c67445dc92f0e649a03600553850d839e3b
ivan <ivan.vecchiato@gmail.com>
2022-01-28 20:20:11 +0100

    inizio sviluppo import/export

--- src/views/ProductConfig/Inventory.vue
+++ src/views/ProductConfig/Inventory.vue
@@ -13,7 +13,7 @@
             type="success"
             size="small"
             plain
-            @click="exportJson">{{ $t("inventory.export") }}</el-button>
+            @click="exportCSV">{{ $t("inventory.export") }}</el-button>
         </el-col>
         <el-col :span="6">
           <input :label="$t('inventory.import')" type="file" @change="loadTextFromFile"/>
@@ -240,32 +240,68 @@ export default {
     randKey: function () {
       return Math.random();
     },
-    exportJson: function () {
-      const data = JSON.stringify(this.products);
-      const blob = new Blob([data], { type: "text/plain" });
-      const e = document.createEvent("MouseEvents"),
-        a = document.createElement("a");
-      a.download = "test.json";
-      a.href = window.URL.createObjectURL(blob);
-      a.dataset.downloadurl = ["text/json", a.download, a.href].join(":");
-      e.initEvent(
-        "click",
-        true,
-        false,
-        window,
-        0,
-        0,
-        0,
-        0,
-        0,
-        false,
-        false,
-        false,
-        false,
-        0,
-        null
-      );
-      a.dispatchEvent(e);
+    exportCSV: function () {
+      var arrData = this.products;
+      var CSV = '';
+
+      //This condition will generate the Label/Header
+      {
+        var row = '';
+
+        //This loop will extract the label from 1st index of on array
+        for (var index in arrData[0]) {
+            //Now convert each value to string and comma-seprated
+            row += index + ';';
+        }
+
+        row = row.slice(0, -1);
+
+        //append Label row with line break
+        CSV += row + '\r\n';
+      }
+
+      //1st loop is to extract each row
+      for (var i = 0; i < arrData.length; i++) {
+          row = '';
+
+          //2nd loop will extract each column and convert it in string comma-seprated
+          for (index in arrData[i]) {
+              row += '"' + arrData[i][index] + '";';
+          }
+
+          row.slice(0, row.length - 1);
+
+          //add a line break after each row
+          CSV += row + '\r\n';
+      }
+
+      if (CSV === '') {
+          alert('Invalid data');
+          return;
+      }
+
+      var ReportTitle = "export";
+
+      //Generate a file name
+      var fileName = '';
+      //this will remove the blank-spaces from the title and replace it with an underscore
+      fileName += ReportTitle.replace(/ /g, '_');
+
+      //Initialize file format you want csv or xls
+      var uri = 'data:text/csv;charset=utf-8,' + escape(CSV);
+      // generate a temp <a /> tag
+      var link = document.createElement('a');
+      link.href = uri;
+
+      //set the visibility hidden so it will not effect on your web-layout
+      link.style = 'visibility:hidden';
+      link.download = fileName + '.csv';
+
+      //this part will append the anchor tag and remove it after automatic click
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+      // console.log('finish download one file');
     },
     loadTextFromFile(ev) {
       const file = ev.target.files[0];
