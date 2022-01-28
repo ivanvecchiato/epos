28a52599d9bbe51fa6bd5cf6c566ee436a46efa3
ivan <ivan.vecchiato@gmail.com>
2022-01-28 11:58:24 +0100

    settaggio area di produzione

--- src/views/Aree/AreeProduzione.vue
+++ src/views/Aree/AreeProduzione.vue
@@ -1,98 +1,116 @@
 <template>
-   <div class="produzione">
+  <div class="produzione">
     <el-row :gutter="20">
-      <el-col :span="4">
-         Nome
-      </el-col>
-      <el-col :span="4">
-        IP stampante
-      </el-col>
+      <el-col :span="4"> Nome </el-col>
+      <el-col :span="4"> IP stampante </el-col>
     </el-row>
-      <ul>
-         <li v-for="area in aree" :key="area.id">
-            <parametri-area
-               :data="area"
-               @saveArea="saveArea"
-               @deleteArea="deleteArea"></parametri-area>
-         </li>
-      </ul>
-      <parametri-area
-         @saveArea="saveArea"
-         @deleteArea="deleteArea"
-         v-if="editingNew">
-      </parametri-area>
-      <el-button size="small" type="primary" plain @click="newArea">+ Aggiungi</el-button>
-
-   </div>
+    <ul>
+      <li v-for="area in aree" :key="area.id">
+        <parametri-area
+          :data="area"
+          :local="isLocalArea(area)"
+          @saveArea="saveArea"
+          @deleteArea="deleteArea"
+          @removeLocal="removeLocal"
+          @setLocal="setLocal">
+        </parametri-area>
+      </li>
+    </ul>
+    <parametri-area
+      @saveArea="saveArea"
+      @deleteArea="deleteArea"
+      v-if="editingNew">
+    </parametri-area>
+    <el-button size="small" type="primary" plain @click="newArea">+ Aggiungi</el-button>
+  </div>
 </template>
 
 <script>
 import Firebase from "../../firebase.js";
-import ParametriArea from "./ParametriArea.vue"
+import ParametriArea from "./ParametriArea.vue";
 
 export default {
-   data() {
-      return {
-         aree: [],
-         editingNew: false
-      }
-   },
-   components: {ParametriArea},
-   methods: {
-      loadAreas() {
-         Firebase.db
-           .collection("aree")
-           .orderBy("id")
-           .onSnapshot((snapshotChange) => {
-             this.aree = [];
-             snapshotChange.forEach((doc) => {
-               var record = doc.data();
-               record.id = doc.id;
-               this.aree.push(record);
-             });
-            });
-      },
-      saveArea(area) {
-         if(area.id == 0) {
-            Firebase.db.collection('aree')
-               .add(Object.assign({}, area))
-               .then((docRef) => {
-                  console.log("Document written with ID: ", docRef.id);
-                  this.editingNew = false;
-               })
-               .catch((error) => {
-                  console.error("Error adding document: ", error);
-               });
-         } else {
-            var ref = Firebase.db.collection("aree").doc(area.id);
-            ref.update({
-               name: area.name,
-               printerAddress: area.printerAddress
-            })
+  data() {
+    return {
+      aree: [],
+      editingNew: false,
+    };
+  },
+  components: { ParametriArea },
+  methods: {
+    isLocalArea(area) {
+      var param = JSON.parse(localStorage.getItem('local_area'));
+      if(param == null) return false;
+      
+      if(param.id == area.id &&
+         param.name == area.name &&
+         param.printerAddress == area.printerAddress) {
+           return true;
          }
-      },
-      newArea() {
-         this.editingNew = true;
-      }
-   },
-   mounted() {
+      else return false;
+    },
+    setLocal(obj) {
+      localStorage.setItem('local_area', JSON.stringify(obj));
       this.loadAreas();
-   },
-}
+    },
+    removeLocal() {
+      localStorage.removeItem('local_area');
+    },
+    loadAreas() {
+      Firebase.db
+        .collection("aree")
+        .orderBy("id")
+        .onSnapshot((snapshotChange) => {
+          this.aree = [];
+          snapshotChange.forEach((doc) => {
+            var record = doc.data();
+            record.id = doc.id;
+            this.aree.push(record);
+          });
+        });
+    },
+    saveArea(area) {
+      if (area.id == 0) {
+        Firebase.db
+          .collection("aree")
+          .add(Object.assign({}, area))
+          .then((docRef) => {
+            console.log("Document written with ID: ", docRef.id);
+            this.editingNew = false;
+          })
+          .catch((error) => {
+            console.error("Error adding document: ", error);
+          });
+      } else {
+        var ref = Firebase.db.collection("aree").doc(area.id);
+        ref.update({
+          name: area.name,
+          printerAddress: area.printerAddress,
+        });
+      }
+    },
+    newArea() {
+      this.editingNew = true;
+    },
+  },
+  mounted() {
+    this.loadAreas();
+  },
+};
 </script>
 
 <style scoped>
 .produzione {
-   text-align: left;
+  text-align: left;
 }
 ul {
   list-style-type: none;
-	list-style: none;
-	padding: 0;
+  list-style: none;
+  padding: 0;
 }
 ul li {
-   margin: 10px;
-	border-left: 4px solid rgb(216, 43, 43);
-   text-align: left;
+  margin: 10px;
+  border-left: 4px solid rgb(216, 43, 43);
+  text-align: left;
 }
 </style>
--- src/views/Aree/ParametriArea.vue
+++ src/views/Aree/ParametriArea.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="row">
-    <el-row :gutter="20">
+    <el-row :gutter="20" align="middle">
       <el-col :span="4">
         <el-input id="f1" v-model="name" placeholder="name" />
       </el-col>
       <el-col :span="4">
         <el-input id="f2" v-model="printerAddress" placeholder="IP Stampante" />
+      </el-col>
+      <el-col :span="2">
+        <el-checkbox v-model="isLocal" @change="localChecked"></el-checkbox>
       </el-col>
       <el-col :span="4">
         <el-button
@@ -14,16 +17,16 @@
           icon="el-icon-check"
           circle
           plain
-          @click="saveArea"
-        ></el-button>
+          @click="saveArea">
+        </el-button>
         <el-button
           type="danger"
           size="small"
           icon="el-icon-delete"
           circle
           plain
-          @click="deleteArea"
-        ></el-button>
+          @click="deleteArea">
+        </el-button>
       </el-col>
     </el-row>
   </div>
@@ -32,12 +35,13 @@
 <script>
 export default {
   name: "ParametriArea",
-  props: ["data"],
+  props: ["data", "local"],
   data() {
     return {
       id: 0,
       name: "",
       printerAddress: "",
+      isLocal: false
     };
   },
   methods: {
@@ -45,18 +49,32 @@ export default {
       this.$emit("saveArea", {
         id: this.id,
         name: this.name,
-        printerAddress: this.printerAddress,
+        printerAddress: this.printerAddress
       });
     },
     deleteArea() {
-      this.$emit("deleteArea", this);
+      this.$emit("deleteArea", this.id);
     },
+    localChecked(event) {
+      if(event) {
+        this.$emit("setLocal", {
+          id: this.id,
+          name: this.name,
+          printerAddress: this.printerAddress
+        });
+      } else {
+        this.$emit("removeLocal")
+      }
+    }
   },
   mounted() {
     if (this.data != undefined) {
       this.id = this.data.id;
       this.name = this.data.name;
       this.printerAddress = this.data.printerAddress;
+    }
+    if(this.local != undefined) {
+       this.isLocal = this.local;
     }
   },
 };
