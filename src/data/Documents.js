bada6a0c580bdb5e728efa01dda73c0514b3139c
ivan <ivan.vecchiato@gmail.com>
2022-02-16 11:30:23 +0100

    tipi di documento (cablati)

---
+++ src/data/Documents.js
@@ -0,0 +1,23 @@
+
+
+const docs = [
+  {
+    id: 1,
+    type: "docs.documento-commerciale"
+  },
+  {
+    id: 2,
+    type: "docs.documento-intestato"
+  },
+  {
+    id: 3,
+    type: "docs.preconto"
+  },
+  {
+    id: 3,
+    type: "docs.estratto-conto"
+  }
+]
+export default {
+  documents: docs
+}
