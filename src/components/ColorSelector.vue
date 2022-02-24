498fd189a8da28611b3a18975bfc2d7d34fe8da0
ivan <ivan.vecchiato@gmail.com>
2022-02-24 22:45:04 +0100

    evidenziazione colore selezionato

--- src/components/ColorSelector.vue
+++ src/components/ColorSelector.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="selector">
+  <div class="selector" v-if="loaded == true">
     <div
       class="color-button"
       :style="getBgc(item)"
-      v-for="item, index in colors" :key="item"
+      v-for="item, index in colors" :key="index"
       :class="{ active : active_el == index }"
       @click="selectColor(item, index)"
     />
@@ -14,15 +14,16 @@
 
 export default {
   name: "ColorSelector",
-  props: ["initialColor"],
+  props: ['initialColor'],
   data() {
     return {
-      colors: ['#00AAE6','#2B73FD','#6F4EFF','#9F41F1','#9E2FBD','#DC5CDC','#EA489C','#FF83A8',
+      colors: ['#00AAE6','#2B73FD','#6F4EFF','#9F41F1','#8D6E63','#DC5CDC','#EA489C','#FF83A8',
       '#D70050','#EB3D34','#F98E2D','#F6CD2D','#93CB00','#61D16F','#39C5AB','#87989F'],
       bgc: {
         backgroundColor: '#fff'
       },
-      active_el: -1
+      active_el: -1,
+      loaded: false
     };
   },
   methods: {
@@ -44,6 +45,7 @@ export default {
         break;
       }
     }
+    this.loaded = true;
   },
 };
 </script>
