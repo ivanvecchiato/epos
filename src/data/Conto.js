77fd435505a5c24b582e94c9a82e613984b74990
ivan <ivan.vecchiato@gmail.com>
2022-03-01 11:05:06 +0100

    field chiusuraFiscale

--- src/data/Conto.js
+++ src/data/Conto.js
@@ -27,7 +27,7 @@ export default class Conto {
       value: 0
     };
     this.status = 0;    // 0=open, 1=closed, -100=deleted, 100=prebill
-    this.chiusura = 0;
+    this.chiusuraFiscale = 0;
     this.source = 0;    //0=frontend, 1000 = takeaway, 2000 = ecommerce, {obj}=tavolo
     this.customer = null;   // class Customer TBD
   }
