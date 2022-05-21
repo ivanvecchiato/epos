df7893bc4c88ac54249a5ab2bb99674c7f3c90bb
ivan <ivan.vecchiato@gmail.com>
2022-05-21 09:24:35 +0200

    file di configurazione locale

---
+++ src/store/configs.js
@@ -0,0 +1,13 @@
+var configs = {
+   fiscal: false,
+   printer: {
+      "address": "192.168.1.101",
+      "port": 9100
+   },
+   emulator: {
+      "address": "127.0.0.1",
+      "port": 9100
+   }
+ }
+
+ export default configs;
