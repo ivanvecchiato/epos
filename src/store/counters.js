e45524fb5285ea81d9ab7e85db9d7dfc183abbd9
ivan <ivan.vecchiato@gmail.com>
2022-05-23 22:51:34 +0200

    aggiunti progressivi e reset

--- src/store/counters.js
+++ src/store/counters.js
@@ -1,6 +1,8 @@
 var counters = {
    progressivoScontrino: 1,
    progressivoProforma: 1,
+   progressivoComanda: 1,
+   progressivoTicket: 1,
    numeroChiusura: 1,
 
    load() {
@@ -10,10 +12,17 @@ var counters = {
       this.numeroChiusura = data.numeroChiusura;
    },
 
+   resetProgressivi() {
+      this.progressivoScontrino = 1;
+      this.progressivoProforma = 1;
+      this.progressivoComanda = 1;
+      this.progressivoTicket = 1;
+   },
+
    getProgressivoScontrino(increment) {
       var p = this.progressivoScontrino;
       if(increment == true) {
-         this.progressivoScontrino++;
+         this.incrementProgressivoScontrino();
       }
       return p;
    },
@@ -22,10 +31,19 @@ var counters = {
       return this.numeroChiusura;
    },
 
+   incrementProgressivoScontrino() {
+      this.progressivoScontrino++;
+      this.save();
+   },
+
    incrementZnum() {
       this.numeroChiusura++;
-   }
+      this.save();
+   },
 
+   save() {
+      localStorage.setItem('counters', JSON.stringify(this))
+   }
 };
 
 export default counters;
