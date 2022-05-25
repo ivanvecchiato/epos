ff845e032baf8f72f704b9925da0a3b422984d0b
ivan <ivan.vecchiato@gmail.com>
2022-05-25 17:56:16 +0200

    aggiunto options[]

--- src/data/Product.js
+++ src/data/Product.js
@@ -28,6 +28,7 @@ export default class Product {
       alarm: 0
     },
     this.components = [];
+    this.options = [];
     this.productionAreas = [];
     this.status = 1;  // 1=active, 0=inactive; -1 deleted
   }
