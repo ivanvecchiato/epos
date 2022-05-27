fbd8d63f035c748ae6cac0ba345a83cff8258477
ivan <ivan.vecchiato@gmail.com>
2022-05-27 17:49:50 +0200

    colori predefiniti

---
+++ src/colors.js
@@ -0,0 +1,44 @@
+var colors = [
+    {
+        id:1,
+        code: '#FFEBEE'
+    },
+    {
+        id:2,
+        code: '#E1F5FE'
+    },
+    {
+        id:3,
+        code: '#E8F5E9'
+    },
+    {
+        id:4,
+        code: '#F3E5F5'
+    },
+    {
+        id:5,
+        code: '#FFF3E0'
+    },
+    {
+        id:6,
+        code: '#F9FBE7'
+    },
+    {
+        id:7,
+        code: '#EFEBE9'
+    },
+    {
+        id:8,
+        code: '#ECEFF1'
+    },
+    {
+        id:9,
+        code: '#E8EAF6'
+    },
+    {
+        id:10,
+        code: '#E0F7FA'
+    }
+]
+
+export default colors
