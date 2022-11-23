e8439bc3ccfeefb7f59397609482d150d524af60
ivan <ivan.vecchiato@gmail.com>
2022-11-23 09:53:23 +0100

    cleanup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,38 +4,47 @@
       <div class="sidemenu" v-if="loggedIn == true">
         <div class="control-bar-user">
           <el-dropdown trigger="click">
-            <img src="@/assets/icons/avatar.png" v-if="avatarLength() > 0"/>
-            <div class="avatar" v-else>{{operator()}}</div>
+            <img src="@/assets/icons/avatar.png" v-if="avatarLength() > 0" />
+            <div class="avatar" v-else>{{ operator() }}</div>
             <template #dropdown>
               <el-dropdown-menu>
-                <el-dropdown-item icon="el-icon-avatar">{{operator()}}</el-dropdown-item>
-                <el-dropdown-item icon="el-icon-right" @click="logout">{{$t('login.logout')}}</el-dropdown-item>
+                <el-dropdown-item icon="el-icon-avatar">{{
+                  operator()
+                }}</el-dropdown-item>
+                <el-dropdown-item icon="el-icon-right" @click="logout">{{
+                  $t("login.logout")
+                }}</el-dropdown-item>
               </el-dropdown-menu>
             </template>
           </el-dropdown>
         </div>
 
-        <div style="width: 100%;display: table;">
+        <div style="width: 100%; display: table">
           <div style="display: table-row">
-          <div class="control-bar-item">
-            <img
-              class="indicator" :class="getPrinterIndicator"
-              src="@/assets/icons/printer.png"/>
-          </div>
-          <div class="control-bar-item">
-            <el-popover
-              placement="right"
-              title="Server locale"
-              :width="200"
-              trigger="click"
-              :content="getServerPopoverContent()">
-              <template #reference>
-                <img
-                  class="indicator" :class="getServerIndicator"
-                  src="@/assets/icons/gear.png"/>
-              </template>
-            </el-popover>
-          </div>
+            <div class="control-bar-item">
+              <img
+                class="indicator"
+                :class="getPrinterIndicator"
+                src="@/assets/icons/printer.png"
+              />
+            </div>
+            <div class="control-bar-item">
+              <el-popover
+                placement="right"
+                title="Server locale"
+                :width="200"
+                trigger="click"
+                :content="getServerPopoverContent()"
+              >
+                <template #reference>
+                  <img
+                    class="indicator"
+                    :class="getServerIndicator"
+                    src="@/assets/icons/gear.png"
+                  />
+                </template>
+              </el-popover>
+            </div>
           </div>
         </div>
         <el-menu
@@ -43,8 +52,9 @@
           @select="handleSelect"
           :default-active="1"
           :collapse="isCollapse"
-          style="text-align:left"
-          active-text-color="#E65100">
+          style="text-align: left"
+          active-text-color="#E65100"
+        >
           <el-menu-item index="1" @click="openDashboard">
             <i class="el-icon-house"></i>
             <span>{{ $t("cashier.dashboard") }}</span>
@@ -80,12 +90,11 @@
           <el-menu-item index="9" @click="openActions" :disabled="!admin()">
             <i class="el-icon-set-up"></i>
             <span>{{ $t("config.actions") }}</span>
-          </el-menu-item>      
-
+          </el-menu-item>
         </el-menu>
       </div>
       <div class="center">
-        <router-view/>
+        <router-view />
       </div>
     </div>
 
@@ -94,10 +103,9 @@
       v-model="showActions"
       :center="false"
       width="40%"
-      destroy-on-close>
-      <frontend-actions>
-      </frontend-actions>
-      
+      destroy-on-close
+    >
+      <frontend-actions> </frontend-actions>
     </el-dialog>
 
     <el-dialog
@@ -106,28 +114,28 @@
       :fullscreen="true"
       :close-on-press-escape="false"
       :show-close="false"
-      destroy-on-close>
-      <authentication @auth="doAuth">
-      </authentication>
-      
+      destroy-on-close
+    >
+      <authentication @auth="doAuth"> </authentication>
     </el-dialog>
   </div>
 </template>
 
 <script>
-import Operator from '../data/Operator.js';
-import operator from "../store/user.js"
-import FrontendActions from "./FrontendActions.vue"
-import Authentication from "./Authentication.vue"
-import geolocation from '../store/geolocation'
+import Operator from "../data/Operator.js";
+import operator from "../store/user.js";
+import FrontendActions from "./FrontendActions.vue";
+import Authentication from "./Authentication.vue";
+import geolocation from "../store/geolocation";
 import Firebase from "../firebase.js";
 import printf from "../fiscalprinter/printf.js";
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    FrontendActions, Authentication
+    FrontendActions,
+    Authentication,
   },
   data() {
     return {
@@ -136,54 +144,50 @@ export default {
       showActions: false,
       printfConnectedStatus: false,
       localServerStatus: false,
-      localServerIP: '0.0.0.0'
-    }
+      localServerIP: "0.0.0.0",
+    };
   },
   computed: {
     loggedOut() {
       return !this.loggedIn;
     },
     getPrinterIndicator() {
-      if(this.printfConnectedStatus == true)
-        return "indicator-green"
-      else
-        return "indicator-red"
+      if (this.printfConnectedStatus == true) return "indicator-green";
+      else return "indicator-red";
     },
     getServerIndicator() {
-      if(this.localServerStatus == true)
-        return "indicator-green"
-      else
-        return "indicator-red"
+      if (this.localServerStatus == true) return "indicator-green";
+      else return "indicator-red";
     },
-    operatorInitial: function() {
+    operatorInitial: function () {
       return operator.getName().charAt(0);
     },
   },
   methods: {
-    admin: function() {
+    admin: function () {
       return operator.isAdmin();
     },
-    getServerPopoverContent: function() {
-      if(this.localServerStatus == true) {
-        return this.localServerIP
+    getServerPopoverContent: function () {
+      if (this.localServerStatus == true) {
+        return this.localServerIP;
       } else {
-        return "server non disponibile"
+        return "server non disponibile";
       }
     },
     doAuth(user) {
       operator.setUserLogged({
         loggedIn: true,
-        operator: user
+        operator: user,
       });
 
       console.log("setOperator", operator);
       this.$message({
-        message: 'Welcome ' + operator.getName(),
-        type: 'success'
+        message: "Welcome " + operator.getName(),
+        type: "success",
       });
 
       this.loggedIn = true;
-      this.$bus.trigger('setOperator', operator)
+      this.$bus.trigger("setOperator", operator);
     },
     handleSelect(key, keyPath) {
       console.log(key, keyPath);
@@ -191,75 +195,78 @@ export default {
     operator() {
       return operator.getName();
     },
-    avatarLength: function() {
-      if(operator.getAvatar() == undefined) return 0
-      else return operator.getAvatar().length
+    avatarLength: function () {
+      if (operator.getAvatar() == undefined) return 0;
+      else return operator.getAvatar().length;
     },
-    logged: function() {
+    logged: function () {
       return operator.isLogged();
     },
-    logout: function() {
+    logout: function () {
       this.loggedIn = false;
       operator.logoutUser();
       //this.$emit('logout')
     },
-    openFloor: function() {
+    openFloor: function () {
       this.$router.push("/floor");
     },
-    openDashboard: function() {
+    openDashboard: function () {
       this.$router.push("/dashboard");
     },
-    openFrontend: function() {
+    openFrontend: function () {
       this.$router.push("/frontend");
     },
-    openOrders: function() {
+    openOrders: function () {
       this.$router.push("/ordini");
     },
-    openBills: function() {
+    openBills: function () {
       this.$router.push("/conti");
     },
-    openProducts: function() {
+    openProducts: function () {
       this.$router.push("/prodconfig");
     },
-    openSettings: function() {
+    openSettings: function () {
       this.$router.push("/settings");
     },
-    openExtra: function() {
+    openExtra: function () {
       this.$router.push("/extras");
     },
-    openActions: function() {
+    openActions: function () {
       this.showActions = true;
     },
-    checkAuth: function() {
-      var data = JSON.parse(localStorage.getItem('user'));
-      if(data == null) {
+    checkAuth: function () {
+      var data = JSON.parse(localStorage.getItem("user"));
+      if (data == null) {
         this.loggedIn = false;
         //this.$router.push("/login");
       } else {
-        var op = new Operator;
+        var op = new Operator();
         op.fillData(data);
         operator.setUserLogged({
           loggedIn: true,
-          operator: op
+          operator: op,
         });
         this.loggedIn = true;
         this.$router.push("/dashboard");
       }
     },
     getGeolocation() {
-        this.gettingLocation = true;
+      this.gettingLocation = true;
       // get position
-      navigator.geolocation.getCurrentPosition(pos => {
-        this.gettingLocation = false;
-        this.location = pos;
-        geolocation.setGeoData(pos);
-      }, err => {
-        this.gettingLocation = false;
-        this.errorStr = err.message;
-      })
+      navigator.geolocation.getCurrentPosition(
+        (pos) => {
+          this.gettingLocation = false;
+          this.location = pos;
+          geolocation.setGeoData(pos);
+        },
+        (err) => {
+          this.gettingLocation = false;
+          this.errorStr = err.message;
+        }
+      );
     },
     playSound() {
-      var audio = new Audio(require('@/assets/bell.mp3'));
+      var audio = new Audio(require("@/assets/bell.mp3"));
       audio.play();
     },
     startOrderMonitor() {
@@ -269,7 +276,7 @@ export default {
         .orderBy("timestamp")
         .onSnapshot((snapshotChange) => {
           this.playSound();
-         // eslint-disable-next-line no-unused-vars
+          // eslint-disable-next-line no-unused-vars
           snapshotChange.forEach((doc) => {
             //var record = doc.data();
             //console.log('orderMonitor', record);
@@ -280,10 +287,10 @@ export default {
       printf.init();
       printf.getStatus((resp) => {
         //console.log('testPrintf', resp)
-        if(resp.result == 'ok') {
+        if (resp.result == "ok") {
           var data = resp.data;
-          if(data.Service.ECRStatus[0].mode != 'REG') {
-            printf.setKey('REG');
+          if (data.Service.ECRStatus[0].mode != "REG") {
+            printf.setKey("REG");
           } else {
             printf.clear();
           }
@@ -298,39 +305,47 @@ export default {
     testLocalServer() {
       var self = this;
 
-      axios.get('http://localhost:8088/test')
+      axios
+        .get("http://localhost:8088/test")
         .then(function (response) {
-          console.log('testLocalServer', response.data);
+          //console.log('testLocalServer', response.data);
           self.localServerIP = response.data.ip;
           self.localServerStatus = true;
         })
         .catch(function (error) {
-          console.log(error);
+          //console.log(error);
+          error;
           self.localServerStatus = false;
-      });
-    }
+        });
+    },
   },
-  mounted() {    
-    if(("geolocation" in navigator)) {
-      console.log('geolocation available');
+  mounted() {
+    if ("geolocation" in navigator) {
+      console.log("geolocation available");
       this.getGeolocation();
     }
 
     this.checkAuth();
     console.log("Home", "setting event listener");
     this.testPrintf();
-    setInterval(function () {
-      this.testPrintf();
-    }.bind(this), 15000); 
+    setInterval(
+      function () {
+        this.testPrintf();
+      }.bind(this),
+      15000
+    );
 
     this.testLocalServer();
-    setInterval(function () {
-      this.testLocalServer();
-    }.bind(this), 16000); 
+    setInterval(
+      function () {
+        this.testLocalServer();
+      }.bind(this),
+      16000
+    );
 
     this.startOrderMonitor();
   },
-}
+};
 </script>
 
 <style scoped>
@@ -358,11 +373,11 @@ export default {
   margin-left: 20px;
 }
 .avatar {
-  color: #A78658;
+  color: #a78658;
   margin: 2px;
   font-size: 1.1em;
   font-weight: normal;
-  background: #EFEBE9;
+  background: #efebe9;
   border: solid 0px white;
   border-radius: 4px;
   padding: 2px 10px 2px 10px;
@@ -386,6 +401,6 @@ export default {
   background-color: var(--danger-color);
 }
 .indicator-green {
-  background-color: #4CAF50;
+  background-color: #4caf50;
 }
 </style>
